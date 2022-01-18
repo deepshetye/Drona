d768becdb2dcfbebf30b3f0c11ac5985c44083de
deepshetye <deepshetye1@gmail.com>
2022-01-18 17:57:53 +0530

    -a

---
+++ 22.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    while (i != 0)
+    {
+        printf("hi\n");
+    }
+
+    do
+    {
+        printf("%d\n", i);
+    } while (i != 0);
+
+    return 0;
+}
